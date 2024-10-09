56c062dff816512396d532408c7afb04c2a1c157
Sahil Jain <sahiljainsj09@gmail.com>
2024-10-10 02:14:13 +0530

    home corrected

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import { Navbar } from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import { Routes, Route } from "react-router-dom";
